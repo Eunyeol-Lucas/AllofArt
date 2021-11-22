143696f39e78b52c40397ae0f2951e4d9746ad96
gon2gon2 <ark5354@gmail.com>
2021-11-22 18:44:44 +0900

    test precommit

--- app/main.py
+++ app/main.py
@@ -1,20 +1,21 @@
-from fastapi import FastAPI
 from endpoints import users
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def test_root():
     return {"Hello": "Main"}
 
+
 @app.get("/api")
-def read_root():
+def test_api_prefix():
     return {"Hello": "api"}
+
 
 # app.mount("/api", user.user)
 app.include_router(users.router, prefix="/api/users")
 # app.include_router(user.user, prefix="/api")
 # app.include_router(user.user, prefix="/api")
 # app.include_router(user.user, prefix="/api")
-
