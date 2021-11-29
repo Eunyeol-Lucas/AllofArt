f2b930fd4e0729aa1b68870534a2cb81981b9325
gon2gon2 <ark5354@gmail.com>
2021-11-29 15:41:28 +0900

    #9 feat: 요청 시 저장된 원본이미지와 전이된 이미지 경로 반환

--- app/ai/style_trs/main.py
+++ app/ai/style_trs/main.py
@@ -5,24 +5,36 @@ import tensorflow as tf
 import tensorflow_hub as hub
 
 
-def tf_read(image: str):
-    img = tf.io.decode_image(image, channels=3, dtype=tf.float32)[tf.newaxis, ...]
+def tf_read(image_path: str):
+    img = tf.io.decode_image(tf.io.read_file(image_path), channels=3, dtype=tf.float32)[
+        tf.newaxis, ...
+    ]
     return img
 
 
-def transfer_style(content_image: str, style_image: str, save_path: str) -> BytesIO:
+def transfer_style(
+    content_image_path: str, style_image_path: str, save_path: str
+) -> BytesIO:
+    try:
+        content_image = tf_read(content_image_path)
+        style_image = tf_read(style_image_path)
 
-    # hub_module = hub.load('style_transfer')
-    hub_handle = "https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2"
-    hub_module = hub.load(hub_handle)
+        # hub_module = hub.load('style_transfer')
+        hub_handle = (
+            "https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2"
+        )
+        hub_module = hub.load(hub_handle)
 
-    outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
-    outputs = outputs[0][0].numpy()
+        outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
+        outputs = outputs[0][0].numpy()
 
-    tf.keras.utils.save_img("test.jpg", outputs)
+        tf.keras.utils.save_img(save_path, outputs)
+        return {
+            "status": "successed",
+            "transfer_image_path": save_path,
+            "content_image_path": content_image_path,
+            "style_image_path": style_image_path,
+        }
 
-    # cv2img = imread('test.jpg')
-    # res, im_jpg = imencode(".jpg", cv2img)
-    # encoded_image = BytesIO(im_jpg.tobytes())
-
-    # return encoded_image
+    except:
+        return {"status": "failed"}
---
+++ app/endpoints/transfer.py
@@ -0,0 +1,61 @@
+from fastapi import APIRouter, File, UploadFile
+from fastapi.responses import HTMLResponse
+
+from app.ai.style_trs.main import transfer_style
+
+router = APIRouter()
+
+
+@router.get("/", response_class=HTMLResponse)
+async def trs_test():
+    return """
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form action="http://localhost:8000/api/style" method="post" enctype="multipart/form-data">
+            <input type="file" name="content_file">
+            <input type="file" name="style_file">
+            <input type="submit">
+        </form>
+    </body>
+</html>
+
+"""
+
+
+@router.post("/")
+async def trs_style(
+    content_file: UploadFile = File(...), style_file: UploadFile = File(...)
+):
+
+    USER_IMAGE_DIR = "/static/user"
+
+    PAINTING_ID = "test"
+
+    # 확장자 check
+    extensions = [
+        file.filename.split(".")[-1].lower() for file in [content_file, style_file]
+    ]
+    for extension in extensions:
+        if extension not in ("jpg", "jpeg", "png"):
+            return "Image must be jpg or png format!"
+
+    content_file_path = f"{USER_IMAGE_DIR}/{PAINTING_ID}_0.jpg"
+    style_file_path = f"{USER_IMAGE_DIR}/{PAINTING_ID}_1.jpg"
+    save_file_path = f"{USER_IMAGE_DIR}/{PAINTING_ID}_2.jpg"
+
+    # 업로드된 파일 저장
+    with open(content_file_path, "wb+") as file_object:
+        file_object.write(content_file.file.read())
+
+    with open(style_file_path, "wb+") as file_object:
+        file_object.write(style_file.file.read())
+
+    result = transfer_style(content_file_path, style_file_path, save_file_path)
+
+    if result["status"] == "failed":
+        return "error!"
+
+    return result["image_path"]
