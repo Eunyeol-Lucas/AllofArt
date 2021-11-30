75e148873cea871709413219efd73c5a74bb00ba
gon2gon2 <ark5354@gmail.com>
2021-11-30 15:45:22 +0900

    #18 feat: gallery endpoint 추가

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.endpoints import register, style, transfer, users
+from app.endpoints import gallery, register, style, transfer, users
 
 app = FastAPI()
 
@@ -34,5 +34,5 @@ app.include_router(users.router, prefix="/api/users")
 app.include_router(style.router, prefix="/api/style")
 app.include_router(transfer.router, prefix="/api/transfer")
 app.include_router(register.router, prefix="/api/register")
-# app.include_router(user.user, prefix="/api")
+app.include_router(gallery.router, prefix="/api/gallery")
 # app.include_router(user.user, prefix="/api")
