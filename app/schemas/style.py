76aa08dcac70bc8365fceae39342a4c0776d0ca4
gon2gon2 <ark5354@gmail.com>
2021-12-01 15:17:29 +0900

    #26 feat: add style schema

---
+++ app/schemas/style.py
@@ -0,0 +1,43 @@
+from typing import Dict
+
+from pydantic import BaseModel
+
+
+class StylePostRequest(BaseModel):
+    file: bytes
+
+
+class StyleBase(BaseModel):
+    painting_id: int = 3
+
+
+class StyleCreate(StyleBase):
+    id: int
+
+
+class Style(StyleCreate):
+
+    artist_id_1: int
+    score_1: float
+    artist_id_2: int
+    score_2: float
+    artist_id_3: int
+    score_3: float
+    artist_id_4: int
+    score_4: float
+    artist_id_5: int
+    score_5: float
+
+    class Config:
+        orm_mode = True
+
+
+class StylePostResponse(StyleBase):
+    style_result: Dict = {
+        "Andy Warhol": 94.77,
+        "Rene Magritte": 5.16,
+        "Henri Matisse": 0.07,
+        "Albrecht Du rer": 0.0,
+        "Alfred Sisley": 0.0,
+    }
+    image_url: str = "/static/images/1.jpg"
