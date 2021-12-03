dc7f783e4afe7308b372b209ded06ae9e22ce584
gon2gon2 <ark5354@gmail.com>
2021-12-03 23:20:33 +0900

    #35 fix: CORS middleware를 starlette의 CORS로 교체

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 from app.endpoints import gallery, register, style, transfer, users
 
