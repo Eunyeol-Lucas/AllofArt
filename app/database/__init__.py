26c1903018f1298553c0933f3354153de0c930a9
gon2gon2 <ark5354@gmail.com>
2021-12-06 10:32:14 +0900

    #35 feat: 실제 db연동되도록 수정

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -2,11 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = "mysql://root:1234@localhost:3306/image"
+from ..config import SQLALCHEMY_DATABASE_URL
+
 # 다른 곳에서 쓰일 엔진
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 
 # sessionlocal 자체로는 아직 세션이 아님, 인스터스 생성해야 세션
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
