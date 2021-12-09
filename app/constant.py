cd27fdd688aa7c8661ca2f4dcc64334cc0d421de
김기원 <kkw-11@naver.com>
2021-12-09 10:56:02 +0900

    #47 fix: transfer style,content 경로 오류 수정

--- app/constant.py
+++ app/constant.py
@@ -12,9 +12,9 @@ SOURCE_IMG:Final = 400
 
 
 # host static 경로 상수
-CONTENT_IMAGE_DIR:Final = "/static/images/artist"
+CONTENT_IMAGE_DIR:Final = "/static/images/conpic"
 
-STYLE_IMAGE_DIR:Final = "/static/images/conpic"
+STYLE_IMAGE_DIR:Final = "/static/images/artist"
 
 PROFILE_IMAGE_DIR:Final = "/static/images/profile"
 
@@ -25,9 +25,9 @@ USER_IMAGE_DIR:Final = "/static/images/user"
 # docker container static 경로 상수
 DOCKER_WORK_DIR:Final = "/code/app"
 
-DOCKER_CONTENT_IMAGE_DIR:Final = f"{DOCKER_WORK_DIR}/static/images/artist"
+DOCKER_CONTENT_IMAGE_DIR:Final = f"{DOCKER_WORK_DIR}/static/images/conpic"
 
-DOCKER_STYLE_IMAGE_DIR:Final = f"{DOCKER_WORK_DIR}/static/images/conpic"
+DOCKER_STYLE_IMAGE_DIR:Final = f"{DOCKER_WORK_DIR}/static/images/artist"
 
 DOCKER_PROFILE_IMAGE_DIR:Final = f"{DOCKER_WORK_DIR}/static/images/profile"
 
